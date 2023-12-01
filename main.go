f7911dd38582b7692e7485924e6219a797543aaa
Gurkan <hello@gurkz.me>
2023-12-01 16:39:16 +0100

    seperate stuff

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-func main() {
-	rl.InitWindow(800, 450, "pong")
+func createGame(width int32, height int32, title string, fps int32) {
+	rl.InitWindow(width, height, title)
 	defer rl.CloseWindow()
 
-	rl.SetTargetFPS(120)
+	rl.SetTargetFPS(fps)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("pong", 400, 225, 20, rl.Black)
-
+		draw()
 		rl.EndDrawing()
 	}
+}
+
+func draw() {
+	rl.ClearBackground(rl.RayWhite)
+	rl.DrawText("pong", 400, 225, 20, rl.Black)
+}
+
+func main() {
+	createGame(800, 450, "pong", 120)
 }
